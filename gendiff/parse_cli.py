0de3b7eae5175c86980effef66ce33b097a3f73c
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-10-16 16:11:28 +0300

    fixs naming

--- gendiff/parse_cli.py
+++ gendiff/parse_cli.py
@@ -2,10 +2,10 @@ import argparse
 
 
 def parse_cli():
-    """Parse command line args and/ or returns tuple filepaths 1, 2 & format.
+    """Parse command line args and/ or returns tuple filepaths 1, 2 & formats.
 
-    :returns: (filepath1, filepath2, format). Tuple of filepath to first_file,
-    second_file and format if any, else default."""
+    :returns: (filepath1, filepath2, formats). Tuple of filepath to first_file,
+    second_file and formats if any, else default."""
 
     available_formats = ['stylish', 'plain', 'json']
 
@@ -19,8 +19,8 @@ def parse_cli():
     parser.add_argument('second_file')
 
     parser.add_argument(
-        '-f', '--format', default='stylish',
-        help=f'''set format of output (default: %(default)s).
+        '-f', '--formats', default='stylish',
+        help=f'''set formats of output (default: %(default)s).
 Formats: {{{", ".join(available_formats)}}}.'''
     )
 
