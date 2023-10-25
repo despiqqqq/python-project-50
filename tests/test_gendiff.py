143e639a88a655ccda665ec47a560e2124e9ea99
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-10-26 00:29:18 +0300

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -40,5 +40,4 @@ def prepared_files(request):
 )
 def test_generate_diff(prepared_files):
     file1_path, file2_path, result_render, format_name = prepared_files
-
     assert result_render == generate_diff(file1_path, file2_path, format_name)
