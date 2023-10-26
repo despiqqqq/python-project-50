e575ee8b2d4bf523619ee509062d6b7eb8919940
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-10-26 23:24:19 +0300

    remake tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,44 +3,30 @@ from gendiff import generate_diff
 import os
 
 
-@pytest.fixture()
-def prepared_files(request):
-    file1_path, file2_path, result_render, format_name = request.param
-    path = os.path.join(os.path.dirname(__file__), 'fixtures')
-    return (os.path.join(path, file1_path), os.path.join(path, file2_path),
-            os.path.join(path, result_render), format_name)
+P1_JSON = 'tests/fixtures/tree1.json'
+P2_JSON = 'tests/fixtures/tree2.json'
+
+P1_YAML = 'tests/fixtures/tree1.yml'
+P2_YAML = 'tests/fixtures/tree2.yml'
+
+PATH_STYLISH = 'tests/fixtures/check_stylish.txt'
+PATH_PLAIN = 'tests/fixtures/check_plain.txt'
+PATH_JSON = 'tests/fixtures/check_json.txt'
+PATH_IDENTICAL_FILES = 'tests/fixtures/check_identical_files.txt'
+
+OPTIONS = [(P1_JSON, P2_JSON, 'stylish', PATH_STYLISH),
+           (P1_JSON, P2_JSON, 'plain', PATH_PLAIN),
+           (P1_JSON, P2_JSON, 'json', PATH_JSON),
+           (P1_YAML, P2_YAML, 'stylish', PATH_STYLISH),
+           (P1_YAML, P2_YAML, 'plain', PATH_PLAIN),
+           (P1_YAML, P2_YAML, 'json', PATH_JSON),
+           (P1_JSON, P1_JSON, 'stylish', PATH_IDENTICAL_FILES),
+           ]
 
 
-@pytest.mark.parametrize(
-    argnames="prepared_files",
-    argvalues=[
-        (
-            'tree1.json',
-            'tree2.json',
-            "tree_json_result.txt",
-            "stylish"
-        ),
-        (
-            'tree1.yml',
-            'tree2.yml',
-            "tree_yaml_result.txt",
-            "stylish"
-        ),
-        (
-            'tree1.json',
-            'tree2.json',
-            "plain_json_result.txt",
-            "plain"
-        ),
-        (
-            'tree1.json',
-            'tree2.json',
-            "json_format_result.json",
-            "json"
-        ),
-    ],
-    indirect=True
-)
-def test_generate_diff(prepared_files):
-    file1_path, file2_path, result_render, format_name = prepared_files
-    assert result_render == generate_diff(file1_path, file2_path, format_name)
+@pytest.mark.parametrize("path1, path2, format, path_check_file", OPTIONS)
+def test_generate_diff(path1, path2, format, path_check_file):
+    res = generate_diff(path1, path2, format)
+
+    with open(path_check_file) as check_file:
+        assert res == check_file.read()
