bfddc9cf7d3635985e971626ee0be2f7ca12bef1
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-10-27 12:56:20 +0300

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,30 +1,38 @@
 import pytest
 from gendiff import generate_diff
 
-P1_JSON = 'tests/fixtures/tree1.json'
-P2_JSON = 'tests/fixtures/tree2.json'
 
-P1_YAML = 'tests/fixtures/tree1.yml'
-P2_YAML = 'tests/fixtures/tree2.yml'
+@pytest.mark.parametrize(
+    argnames="prepared_files",
+    argvalues=[
+        (
+            'tree1.json',
+            'tree2.json',
+            "tree_json_result.txt",
+            "stylish"
+        ),
+        (
+            'tree1.yml',
+            'tree2.yml',
+            "tree_yaml_result.txt",
+            "stylish"
+        ),
+        (
+            'tree1.json',
+            'tree2.json',
+            "plain_json_result.txt",
+            "plain"
+        ),
+        (
+            'tree1.json',
+            'tree2.json',
+            "json_format_result.json",
+            "json"
+        ),
+    ],
+    indirect=True
+)
+def test_generate_diff(prepared_files):
+    file1_path, file2_path, result_render, format_name = prepared_files
 
-PATH_STYLISH = 'tests/fixtures/check_stylish.txt'
-PATH_PLAIN = 'tests/fixtures/check_plain.txt'
-PATH_JSON = 'tests/fixtures/check_json.txt'
-PATH_IDENTICAL_FILES = 'tests/fixtures/check_identical_files.txt'
-
-OPTIONS = [(P1_JSON, P2_JSON, 'stylish', PATH_STYLISH),
-           (P1_JSON, P2_JSON, 'plain', PATH_PLAIN),
-           (P1_JSON, P2_JSON, 'json', PATH_JSON),
-           (P1_YAML, P2_YAML, 'stylish', PATH_STYLISH),
-           (P1_YAML, P2_YAML, 'plain', PATH_PLAIN),
-           (P1_YAML, P2_YAML, 'json', PATH_JSON),
-           (P1_JSON, P1_JSON, 'stylish', PATH_IDENTICAL_FILES),
-           ]
-
-
-@pytest.mark.parametrize("path1, path2, format, path_check_file", OPTIONS)
-def test_generate_diff(path1, path2, format, path_check_file):
-    res = generate_diff(path1, path2, format)
-
-    with open(path_check_file) as check_file:
-        assert res == check_file.read()
+    assert result_render == generate_diff(file1_path, file2_path, format_name)
