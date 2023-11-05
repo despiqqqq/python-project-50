b620fdd806434ee2163fc796eed3d1742cdd0f69
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-11-05 19:19:19 +0300

    Update parse_data.py

--- gendiff/parse_data.py
+++ gendiff/parse_data.py
@@ -1,4 +1,3 @@
-import os
 import json
 import yaml
 
@@ -6,7 +5,7 @@ import yaml
 def get_data(file_path: str):
     with open(file_path, 'r') as file:
         data = file.read()
-        extension = os.path.splitext(file_path)[1]
+        extension = file_path.split(".")[-1]
     return parse_data(data, extension)
 
 
@@ -16,4 +15,4 @@ def parse_data(data, extension):
     elif extension == ".json":
         return json.loads(data)
     else:
-        raise ValueError('Filetype is not supported.')
+        raise ValueError('Тип файла не поддерживается.')
